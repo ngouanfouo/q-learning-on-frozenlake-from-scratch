054e4530fd693157ae36a6f48303789ea473e42e
Durel-J <132607998+ngouanfouo@users.noreply.github.com>
2026-06-19 19:21:54 +0100

    Step 9: td_error

--- model.py
+++ model.py
@@ -65,8 +65,10 @@ def td_target(reward, gamma, q_table, next_state, done):
     else:
         return float(reward + gamma * max_q_value(q_table,next_state))
 
-# Step 9 - td_error (not yet solved)
-# TODO: implement
+# Step 9 - td_error
+def td_error(target, q_table, state, action):
+    # TODO: return the TD error: target minus current Q(state, action)
+    return float(target-q_table[state][action])
 
 # Step 10 - q_learning_update (not yet solved)
 # TODO: implement
